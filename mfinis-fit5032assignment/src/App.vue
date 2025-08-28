16947e5c9e206eb1ff892d83433eb89fddeeda00
matthewfinis <matthew.finis@gmail.com>
2025-08-28 10:33:42 +1000

    continued work on formatting + added footer

--- mfinis-fit5032assignment/src/App.vue
+++ mfinis-fit5032assignment/src/App.vue
@@ -2,21 +2,19 @@
 import { RouterView } from 'vue-router';
 import SiteHeader from './components/SiteHeader.vue';
 import './assets/main.css';
+import SiteFooter from './components/SiteFooter.vue';
 </script>
 
 <template>
-  <div class="display">
-    <SiteHeader />
-    <RouterView class="content"/>
+  <header class="sticky-top"><SiteHeader /></header>
+  <div class="site-content">
+    <RouterView style="background-color: var(--color-secondary);"/>
   </div>
+  <footer class="sticky-bottom"><SiteFooter /></footer>
 </template>
 
 <style scoped>
-.content {
-  height: 75vh;
-  background-color: transparent;
-}
-.display {
-  background-color: var(--color-secondary);
+.site-content {
+  background-color: var(--color-secondary);;
 }
 </style>
