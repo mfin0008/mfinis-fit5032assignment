58191410cdc3f81ff97c5170dfba7129fe4b1360
matthewfinis <matthew.finis@gmail.com>
2025-08-28 13:30:16 +1000

    initial setup of player cards

---
+++ mfinis-fit5032assignment/src/components/PlayerCard.vue
@@ -0,0 +1,74 @@
+<script setup>
+const props = defineProps({
+  firstName: String,
+  nickName: String,
+  lastName: String,
+  position: String,
+  stats: {
+    kicking: Number,
+    handling: Number,
+    goalKicking: Number,
+    marking: Number,
+    pace: Number,
+    defending: Number,
+    strength: Number,
+    tackling: Number
+  }
+});
+
+const statMetadata = {
+  kicking: {
+    pos: {x: "120", y: "520"},
+    name: 'KIC'
+  },
+  handling: {
+    pos: {x: "120", y: "580"},
+    name: 'HND'
+  },
+  goalKicking: {
+    pos: {x: "120", y: "640"},
+    name: 'GOA'
+  },
+  marking: {
+    pos: {x: "120", y: "700"},
+    name: 'MAR'
+  },
+  pace: {
+    pos: {x: "320", y: "520"},
+    name: 'PAC'
+  },
+  defending: {
+    pos: {x: "320", y: "580"},
+    name: 'DEF'
+  },
+  strength: {
+    pos: {x: "320", y: "640"},
+    name: 'STR'
+  },
+  tackling: {
+    pos: {x: "320", y: "700"},
+    name: 'TAC'
+  }
+};
+</script>
+
+<template>
+  <svg class="overlay-svg" viewBox="0 0 600 900" preserveAspectRatio="xMidYMid meet">
+    <image href="@/assets/player-card.png" x="0" y="0" width="600" height="900" />
+    <text
+      v-for="stat in Object.keys(props.stats)"
+      :key="stat"
+      :x="statMetadata[stat].pos.x"
+      :y="statMetadata[stat].pos.y"
+      font-size="40"
+      font-weight="700"
+      fill="var(--color-neutral)"
+    >
+      {{ `${stats[stat]} ${statMetadata[stat].name}` }}
+    </text>
+
+  </svg>
+</template>
+
+<style scoped>
+</style>
--- mfinis-fit5032assignment/src/views/SignUpPlayerView.vue
+++ mfinis-fit5032assignment/src/views/SignUpPlayerView.vue
@@ -1,59 +1,74 @@
 <script setup>
 import '@material/web/all.js';
-import '@material/web/slider/internal/slider.js';
+import '@material/web/slider/slider.js';
 import '../assets/main.css';
 import { ref } from 'vue';
+import PlayerCard from '@/components/PlayerCard.vue';
 
-const sliderValue = ref(50);
+const MAX_STAT_TOTAL = 600;
+const isAtStatTotal = ref(false);
 
-const onInput = (e) => {
-  sliderValue.value = Number((e.target).value);
+const playerStats = ref({
+    kicking: 50,
+    handling: 50,
+    goalKicking: 50,
+    marking: 50,
+    pace: 50,
+    defending: 50,
+    strength: 50,
+    tackling: 50
+  }
+);
+
+const onSliderInput = (event) => {
+  const newValue = Number((event.target).value);
+  const amountOverTotalPermittedStats = getAmountOverTotalPermittedStats(event.target.id, newValue);
+  if (amountOverTotalPermittedStats <= 0) {
+    playerStats.value[event.target.id] = newValue;
+  } else {
+    playerStats.value[event.target.id] = newValue - amountOverTotalPermittedStats;
+  }
 }
+
+const getAmountOverTotalPermittedStats = (statName, newValue) => {
+  let newPlayerStats = {...playerStats.value};
+  newPlayerStats[statName] = newValue;
+  const newStatTotal = Object.values(newPlayerStats).reduce((sum, value) => sum + value, 0);
+
+  isAtStatTotal.value = newStatTotal >= MAX_STAT_TOTAL;
+  return newStatTotal - MAX_STAT_TOTAL;
+}
+
+const resetSlider = (event) => {
+  const statName = event.target.id;
+  event.target.value = playerStats.value[statName];
+}
+
 </script>
 
 <template>
   <div class="container">
     <div class="row pt-5 pb-5">
       <div class="col-1 d-xxl-none"></div>
-      <div class="col-10 col-xxl-4 content-box mb-3">
-        <md-slider></md-slider>
-      </div>
+      <div class="col-10 col-xxl-4 content-box mb-3"></div>
 
       <div class="col-1 col-xxl-1"></div>
 
       <div class="col-1 d-xxl-none"></div>
-      <div class="col-10 col-xxl-2 content-box mb-3 stat-slider">
-        <div class="slider-pair">
-          KICKING
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          HANDLING
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          GOALKICKING
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          MARKING
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          PACE
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          DEFENDING
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          STRENGTH
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
-        </div>
-        <div class="slider-pair">
-          TACKLING
-          <md-slider :value="sliderValue" @input="onInput"></md-slider>
+      <div class="col-10 col-xxl-2 content-box mb-3">
+        <div
+          class="slider-pair"
+          v-for="stat in Object.keys(playerStats)"
+          :key="stat"
+        >
+          {{ String(stat).toUpperCase() }}
+          <md-slider
+            :id="stat"
+            :value="playerStats[stat]"
+            :class="{ 'at-stat-total': isAtStatTotal }"
+            @input="onSliderInput"
+            @change="resetSlider">
+          </md-slider>
         </div>
       </div>
       <div class="col-1 d-xxl-none"></div>
@@ -61,8 +76,11 @@ const onInput = (e) => {
       <div class="col-1 col-xxl-1"></div>
 
       <div class="col-10 col-xxl-4 content-box mb-3">
-        <md-slider></md-slider>
-        {{ sliderValue }}
+        <h2 class="mt-3">Your player card!</h2>
+        <PlayerCard :stats="playerStats"/>
+        <div v-if="isAtStatTotal" class="error-text mt-2">
+          Total reached ({{ MAX_STAT_TOTAL }}).
+        </div>
       </div>
       <div class="col-1 d-xxl-none"></div>
     </div>
@@ -78,14 +96,23 @@ md-slider {
   width: 60%
 }
 
+md-slider.at-stat-total{
+  --md-slider-inactive-track-color: var(--color-primary);
+}
+
 .content-box {
   background-color: var(--color-raised);
   color: var(--color-neutral);
   font-size: x-large;
   text-align: center;
+  align-items: center;
 }
 
 .slider-pair {
   margin-top: 1.5rem;
+}
+
+.error-text {
+  color: var(--color-primary);
 }
 </style>
